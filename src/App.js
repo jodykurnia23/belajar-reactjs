43a3f31417edb8f14d030d7e89f51895f9dd409d
jody_kurnia <jodykurnia2@gmail.com>
2022-11-18 11:57:49 +0700

    Perubahan isi

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br />
-          Oke sudah ditambah.
+          Jody Ganteng
         </p>
         <a
           className="App-link"
