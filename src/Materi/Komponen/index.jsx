11a18873b5ba15aff925cd347acfcfe7192833be
jody_kurnia <jodykurnia2@gmail.com>
2022-11-19 19:24:38 +0700

    Membuat Class Component

---
+++ src/Materi/Komponen/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ClassComponent from "./pembahasan/ClassComponent";
+
+export default class Komponen extends React.Component{
+    render(){
+        return(
+            <div>
+                <ClassComponent />
+            </div>
+        )
+    }
+}
---
+++ src/Materi/Komponen/pembahasan/ClassComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class ClassComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Jody Ganteng</h1>
+                <h2>Komponen ini menggunakan class component</h2>
+            </div>
+        )
+    }
+}
