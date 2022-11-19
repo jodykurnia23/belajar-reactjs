22a3a206b6d3d14e42cc52481d4ac5a17fb96736
jody_kurnia <jodykurnia2@gmail.com>
2022-11-19 18:52:44 +0700

    perubahan pada app

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br />
-          Jody Ganteng
         </p>
         <a
           className="App-link"
