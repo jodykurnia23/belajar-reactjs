c66e28dcb4707b70a61c0ba16073762c0f2eae64
jody_kurnia <jodykurnia2@gmail.com>
2022-11-19 20:29:42 +0700

    Membuat komponen dengan functional component

--- src/Materi/Komponen/index.jsx
+++ src/Materi/Komponen/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ClassComponent from "./pembahasan/ClassComponent";
+import FunctionalComponent from "./pembahasan/FunctionalComponent";
 
 export default class Komponen extends React.Component{
     render() {
         return(
             <div>
                 <ClassComponent />
+                <FunctionalComponent />
             </div>
         )
     }
---
+++ src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
@@ -0,0 +1,3 @@
+const FunctionalComponent = () => <h1>Membuat komponen dengan functional component</h1>;
+
+export default FunctionalComponent;
